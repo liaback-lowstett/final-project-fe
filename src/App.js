f384f7ab2aaa8cab4820ffd68cd4fa8ce79f2bcd
liaback <felicia.liaback@gmail.com>
2021-06-02 15:54:30 +0200

    create pages Home and Task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Tasks from './pages/Tasks'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/home" component={Home}/>
+        <Route path="/tasks" component={Tasks}/>
+      </Switch>
+    </BrowserRouter>
   )
 }
