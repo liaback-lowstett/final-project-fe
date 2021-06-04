c08686f9aba169fa09d796f7c18005343bad178e
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-06-04 07:45:11 +0200

    started with our tasks page

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/macro'
+import { useSelector, useDispatch, batch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
-const Tasks = () =>{
-    return(
-        <div>
-            Tasks
+import tasks from '../reducers/tasks'
+import { BASE_URL } from '../reusables/urls'
+
+const Tasks = () => {
+  const [newTask, setNewTask] = useState('')
+  const taskItems = useSelector((store) => store.tasks.taskItem)
+
+  const dispatch = useDispatch()
+
+  const fetchTasks = () => {
+    const options = {
+      method: 'GET'
+    }
+
+    fetch(BASE_URL('tasks'), options)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(tasks.actions.addTask(data.tasks))
+      })
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ taskItem: newTask })
+    }
+
+    fetch(BASE_URL('tasks'), options)
+      .then((res) => res.json())
+      .then((data) => fetchTasks(data))
+  }
+
+  return (
+    <div>
+      <h1>List</h1>
+      {taskItems.map((task) => (
+        <div key={task._id}>
+          <div>{task.taskItem}</div>
         </div>
-    )
-}
+      ))}
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+};
 
-export default Tasks
+export default Tasks;
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,23 +1,23 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const tasks = createSlice ({
-    name:'tasks',
-    initialState: {
-        taskItem:[],
-        deadline:null,
-        error: null
+const tasks = createSlice({
+  name: 'tasks',
+  initialState: {
+    taskItem: [],
+    deadline: null,
+    error: null
+  },
+  reducers: {
+    addTask: (store, action) => {
+      store.taskItem = action.payload;
     },
-    reducers: {
-        addTask: (store, action) => {
-            store.taskItem = action.payload
-        },
-        addDeadline: (store, action) => {
-            store.deadline = action.payload
-        },
-        setErrors: (store, action) => {
-            store.errors = action.payload
-        }
+    addDeadline: (store, action) => {
+      store.deadline = action.payload;
+    },
+    setErrors: (store, action) => {
+      store.errors = action.payload;
     }
-}) 
+  }
+});
 
-export default tasks
+export default tasks;
