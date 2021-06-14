e804dd0d3b5c803aea256af98e9937ab727c1f93
liaback <felicia.liaback@gmail.com>
2021-06-14 11:54:12 +0200

    style start page

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import './Start.scss'
+import illustration from '../../assets/illustration.png'
 
 const Start = () => {
-  return <div>start screen</div>;
+  return (
+    <div className="start">
+      <div className="img-container">
+        <img src={illustration} alt="illustration"/>
+      </div>
+    </div>
+  )
+  
 };
 
 export default Start;
